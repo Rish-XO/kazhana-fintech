0202159d24e50bc4dab837dadd6a83710890fd7a
Rish-XO <rishal.mhd009@gmail.com>
2025-03-19 12:31:00 +0530

    port change

--- main.py
+++ main.py
@@ -20,5 +20,5 @@ async def root():
     return {"message": "API is running!"}
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 5000))  # Ensure it runs on Koyeb's default port
+    port = int(os.getenv("PORT", 8000))  # Set default to 8000
     uvicorn.run(app, host="0.0.0.0", port=port)
